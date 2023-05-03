224070968bdac8853fa7f9249c2a1b2a65a01a69
Jazelle Cruz <jazellecruz22@gmail.com>
2023-05-03 17:36:11 +0800

    delete files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  DB_NAME: process.env.DB_NAME,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_HOST: process.env.DB_HOST
-}
